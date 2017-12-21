03481f554d7a0a5ff6e42533df9bac17a90a37de
刘振宇 <liuzhenyulive@live.com>
2017-12-21 22:41:35 +0800

    实现Redos的增、改、删、查

--- RedisDotNetDemo/Program.cs
+++ RedisDotNetDemo/Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using ServiceStack;
+using ServiceStack.Text;
+using ServiceStack.Redis;
+using ServiceStack.DataAnnotations;
 
 namespace RedisDotNetDemo
 {
@@ -6,7 +10,31 @@ namespace RedisDotNetDemo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var redisManger = new RedisManagerPool("127.0.0.1:6379");
+            var redis = redisManger.GetClient();
+            var redisTodos = redis.As<Todo>();
+            var  newTodo=new Todo
+            {
+                Id = redisTodos.GetNextSequence(),
+                Content = "Learn Redis",
+                Order = 1,
+            };
+            redisTodos.Store(newTodo);
+            Todo saveTodo = redisTodos.GetById(newTodo.Id);
+            "Saved Todo: {0}".Print(saveTodo.Dump());
+
+            saveTodo.Done = true;
+            redisTodos.Store(saveTodo);
+
+            var updateTodo = redisTodos.GetById(newTodo.Id);
+            "Updated Todo: {0}".Print(updateTodo.Dump());
+
+            redisTodos.DeleteById(newTodo.Id);
+
+            var remainingTodos = redisTodos.GetAll();
+            "No more Todos:".Print(remainingTodos.Dump());
+
+            Console.ReadLine();
         }
     }
 }
