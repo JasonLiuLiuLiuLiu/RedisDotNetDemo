d3e49f64472afb591f470a7142d1262f6b4b0e39
刘振宇 <liuzhenyulive@live.com>
2017-12-22 10:37:48 +0800

    添加了注释

--- RedisDotNetDemo/Program.cs
+++ RedisDotNetDemo/Program.cs
@@ -10,26 +10,26 @@ namespace RedisDotNetDemo
     {
         static void Main(string[] args)
         {
-            var redisManger = new RedisManagerPool("127.0.0.1:6379");
-            var redis = redisManger.GetClient();
+            var redisManger = new RedisManagerPool("127.0.0.1:6379");      //Redis的连接字符串
+            var redis = redisManger.GetClient();                           //获取一个Redis Client
             var redisTodos = redis.As<Todo>();
-            var  newTodo=new Todo
+            var  newTodo=new Todo                                          //实例化一个Todo类
             {
                 Id = redisTodos.GetNextSequence(),
                 Content = "Learn Redis",
                 Order = 1,
             };
-            redisTodos.Store(newTodo);
-            Todo saveTodo = redisTodos.GetById(newTodo.Id);
+            redisTodos.Store(newTodo);                                    //把newTodo实例保存到数据库中    增     
+            Todo saveTodo = redisTodos.GetById(newTodo.Id);               //根据Id查询        查
             "Saved Todo: {0}".Print(saveTodo.Dump());
 
-            saveTodo.Done = true;
+            saveTodo.Done = true;                                         //改
             redisTodos.Store(saveTodo);
 
-            var updateTodo = redisTodos.GetById(newTodo.Id);
+            var updateTodo = redisTodos.GetById(newTodo.Id);            //查
             "Updated Todo: {0}".Print(updateTodo.Dump());
 
-            redisTodos.DeleteById(newTodo.Id);
+            redisTodos.DeleteById(newTodo.Id);                           //删除
 
             var remainingTodos = redisTodos.GetAll();
             "No more Todos:".Print(remainingTodos.Dump());
